3819142eb128e0894546306e5b32dfa2e2e765fd
huangxingzhou <huangxingzhou@karrytech.com>
2019-09-22 22:51:39 +0800

    [IMP]打开debug模式

--- app/config.py
+++ app/config.py
@@ -4,6 +4,7 @@ basedir = os.path.abspath(os.path.dirname(__file__))
 load_dotenv(os.path.join(basedir, '.env'))
 
 class Config(object):
+    DEBUG = True
     SECRET_KEY = os.environ.get('SECRET_KEY')
     SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or \
                               'sqlite:///' + os.path.join(basedir, 'app.db')
